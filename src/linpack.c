28b3bbd7632855fdc6c8abfe38492ce0f4517f51
root <ehab.saleh@lrz.de>
2026-03-15 15:56:00 +0100

    refine linpack.c

--- src/linpack.c
+++ src/linpack.c
@@ -906,45 +906,44 @@ static REAL second(void)
     }
 
 static size_t detect_llc_size(){
-    FILE*f=fopen("/sys/devices/system/cpu/cpu0/cache/index3/size", "r");
-    if(!f){
-        printf("LLC detection failed. Using default 32MB\n");
-        return 32*1024*1024;
-    }
-    size_t size;
-    char unit;
-    fscanf(f,"%zu%c",&size,&unit);
-    if(unit=='K')
-        return size*1024;
-    else if(unit=='M')
-        return size*1024*1024;
-    else
-        return size;
-
+	size_t size;
+	FILE*f=fopen("/sys/devices/system/cpu/cpu0/cache/index3/size", "r");
+    	if(!f){
+        	printf("LLC detection failed. Using system file cpuinfo\n");
+        	size=detect_cache_cpuinfo();
+    	}
+	char unit;
+    	fscanf(f,"%zu%c",&size,&unit);
+    	if(unit=='K')
+        	size=size*1024;
+    	else if(unit=='M')
+        	size=size*1024*1024;
+	fclose(f);
+	return size;
 }
 
 static size_t detect_cache_cpuinfo(){
-    FILE*f=fopen("/proc/cpuinfo","r");
-    if(!f){
-        printf("LLC detection failed. Using default 32MB\n");
-        return 32*1024*1024;
-    }
-
-    char line[256];
-    while(fgets(line,sizeof(line),f)){
-        if(strstr(line,"cache size")) {
-            size_t size;
-            char unit[2];
-            sscanf(line,"cache size : %zu %s",&size,unit);
-            if(strcmp(unit,"KB")==0)
-                return size*1024;
-            else if(strcmp(unit,"MB")==0)
-                return size*1024*1024;
-            else
-                return size;
+	FILE*f=fopen("/proc/cpuinfo","r");
+  	if(!f){
+        	printf("LLC detection failed. Using default 32MB\n");
+        	return 32*1024*1024;
     	}
 
+    	char line[256];
+    	size_t size;
+	while(fgets(line,sizeof(line),f)){
+        	if(strstr(line,"cache size")) {
+            	char unit[2];
+            	sscanf(line,"cache size : %zu %s",&size,unit);
+            	if(strcmp(unit,"KB")==0)
+                	size=size*1024;
+            	else if(strcmp(unit,"MB")==0)
+                	size=size*1024*1024;
+    		}
 	}
+	fclose(f);
+	return size;
+	
 }
 void usage(const char *argv0) {
       fprintf(stderr,
